ecfaae6cc05a13de8959afa2ed7237959f51507d
edgart83 <edgart83@users.noreply.github.com>
2014-05-25 12:01:06 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -5,13 +5,13 @@
 		
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL				# place holder for the inverse matrix
-        set <- function(y) {	# establishes the value of the matrix
-                x <<- y			# x now contains the matrix 
+        set <- function(y) {			# establishes the value of the matrix
+                x <<- y				# x now contains the matrix 
                 m <<- NULL
         }
-        get <- function() x							# get the matrix
+        get <- function() x				# get the matrix
         setinvmtx <- function(solve) m <<- solve	# populate (m) with the inv matrix 
-        getinvmtx <- function() m					# get the inverse matrix (m)
+        getinvmtx <- function() m			# get the inverse matrix (m)
         list(set = set, get = get,
              setinvmtx = setinvmtx,
              getinvmtx = getinvmtx)
@@ -27,14 +27,14 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 		m <- x$getinvmtx()
-        if(!is.null(m)) {							## if place holder not m is empty 
-													## then solution exists
+        if(!is.null(m)) {						## if place holder not m is empty 
+									## then solution exists
                 message("getting cached data")
-                return(m)							## inverse matrix is returned
+                return(m)						## inverse matrix is returned
         }
-		## Need to compute inverse matrix as solution does not exists
-        data <- x$get()								## get the matrix
+	## Need to compute inverse matrix as solution does not exists
+        data <- x$get()							## get the matrix
         m <- solve(data, ...)						## Compute inverse matrix
-        x$setinvmtx(m)								## Store result in (m)
-        m									## return the value of the invertse matrix
+        x$setinvmtx(m)							## Store result in (m)
+        m								## return the value of the invertse matrix
 }
